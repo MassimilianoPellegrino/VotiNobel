a021dfc6441087e756e763097b8b6dcf765841db
lenovo <lenovo@LAPTOP-2NKIQS9D.homenet.telecomitalia.it>
2021-04-14 15:23:00 +0200

    1

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,15 +1,88 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+import it.polito.tdp.nobel.db.EsameDAO;
 
 public class Model {
 
+	private List<Esame> partenza;
+	private Set<Esame> soluzioneMigliore;
+	private double mediaSoluzioneMigliore;
+	
+	public Model() {
+		EsameDAO dao = new EsameDAO();
+		this.partenza = dao.getTuttiEsami();
+	}
 	
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
-		System.out.println("TODO!");
-		return null;	
+		Set<Esame> parziale = new HashSet<>();
+		soluzioneMigliore = new HashSet<>();
+		mediaSoluzioneMigliore=0.0;
+		
+		//cerca1(parziale, 0, numeroCrediti);
+		cerca2(parziale, 0, numeroCrediti);
+		
+		return soluzioneMigliore;	
+	}
+	
+	private void cerca1(Set<Esame> parziale, int L, int m) {
+		int crediti=sommaCrediti(parziale);
+		
+		if(crediti>m) {
+			return;
+		}
+		
+		if(crediti==m) {
+			double media = calcolaMedia(parziale);
+			if(media>mediaSoluzioneMigliore) {
+				soluzioneMigliore = new HashSet<>(parziale);
+				mediaSoluzioneMigliore = media;
+			}
+			return;
+		}
+		
+		if(L==partenza.size()) {
+			return;
+		}
+		
+		for(Esame e: partenza) {
+			if(!parziale.contains(e)) {
+				parziale.add(e);
+				cerca1(parziale, L+1, m);
+				parziale.remove(e);
+			}
+		}
 	}
 
+	private void cerca2(Set<Esame> parziale, int L, int m) {
+		int crediti=sommaCrediti(parziale);
+		
+		if(crediti>m) {
+			return;
+		}
+		
+		if(crediti==m) {
+			double media = calcolaMedia(parziale);
+			if(media>mediaSoluzioneMigliore) {
+				soluzioneMigliore = new HashSet<>(parziale);
+				mediaSoluzioneMigliore = media;
+			}
+			return;
+		}
+		
+		if(L==partenza.size()) {
+			return;
+		}
+		
+		parziale.add(partenza.get(L));
+		cerca2(parziale, L+1, m);
+		
+		parziale.remove(partenza.get(L));
+		cerca2(parziale, L+1, m);
+	}
 	
 	public double calcolaMedia(Set<Esame> esami) {
 		
